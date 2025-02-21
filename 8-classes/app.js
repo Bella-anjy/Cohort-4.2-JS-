4c4546a5ea139320d15dff8a09250d2900ad50f1
Olubiyo Aliyah <aliyaholubiyo2008@gmail.com>
2025-02-21 11:13:27 +0100

    OOP note

--- 8-classes/app.js
+++ 8-classes/app.js
@@ -1,3 +1,154 @@
+ /**
+ * Introduction to Classes
+ * An object-oriented programming (OOP) in JavaScript
+ * can be used to model real-world examples
+ * To create a class, we write the class keyword
+ * followed by class name with the first letter
+ * capitalize
+ */
+
+//iilustration
+// class ClassName {}
+// For instance , to create a student record class
+
+// class StudentProfile {}
+
+/**
+ * Constructor
+ * Constructor is the method JavaScript calls every time
+ * it creates a new instance of a class.
+ * When using this inside a constructor function, it refers to the new instance of the object created
+ * by the constructor.
+ */
+
+class StudentProfile {
+  constructor(name, courseLevel, age, address, gender, matricNumber) {
+    this.name = name;
+    this.courseLevel = courseLevel;
+    this.age = age;
+    this.address = address;
+    this.gender = gender;
+    this.matricNumber = matricNumber;
+  }
+}
+
+/**
+ * Instance of a Class
+ * An instance is an object that contains the
+ * property names and methods of a class,
+ * but with unique property values.
+ * To create a instance of a class, we use the 'new' keyword
+ * followed by classname with parentheses.
+ * The 'new' keyword calls the constructor(), runs the code
+ * inside of it, and then returns the new instance.
+ */
+const alimiProfile = new StudentProfile(
+  "Alimi Kehinde",
+  "300L",
+  25,
+  "1200, Orinla Avenue, off Beesam",
+  "Male",
+  9087654
+);
+console.log("alimiProfile", alimiProfile);
+
+// Class work:
+// Create a JS class named HospitalRecord, add the following parameters to the constructor object/method firstname, lastname, wardNumber, addmissionDate, discahrgeDate, hospitalName, and
+// patientIllness. Create atmost three instance of the HospitalRecord class.
+
+/**
+ * Class Method, we are going to use
+ * the 'get' keyword with property names
+ * followed by a parentheses and curly
+ * brace. The property names will be
+ * prepended with underscores
+ * which indicate these properties should not
+ * be accessed directly(private properties).
+ */
+
+class Garage {
+  constructor(
+    carMake,
+    carColor,
+    carBrand,
+    plateNumber,
+    engineNumber,
+    carModel,
+    chasisNumber
+  ) {
+    this._carMake = carMake;
+    this._carBrand = carBrand;
+    this._plateNumber = plateNumber;
+    this._engineNumber = engineNumber;
+    this._carModel = carModel;
+    this._chasisNumber = chasisNumber;
+    this._carColor = carColor;
+    this._carTotal = 2000;
+  }
+
+  get carManufacturer() {
+    return this._carMake;
+  }
+  get carAppearance() {
+    return this._carColor;
+  }
+  get carIdentity() {
+    return this._chasisNumber;
+  }
+  get getVersion() {
+    return this._carModel;
+  }
+  get totalNumberOfCars() {
+    return this._carTotal;
+  }
+  currentCarTotal() {
+    return (this._carTotal = this._carTotal - 500);
+  }
+  addMoreCars() {
+    return this._carTotal++;
+  }
+}
+
+const toyotaCamry = new Garage(
+  "Toyota",
+  "Black",
+  "Camry",
+  "LAG 123 MUS",
+  "EA12345678999",
+  "2021",
+  "JFZ12U23U18864"
+);
+console.log("toyotaCamry==>", toyotaCamry);
+// total cars in garage
+// add new cars
+let addMoreCars = toyotaCamry.addMoreCars();
+console.log("toyotaCamry", toyotaCamry._carTotal);
+
+// get current car total
+console.log("currentCarTotal", toyotaCamry.currentCarTotal());
+
+// get car color
+
+let color = toyotaCamry.carAppearance;
+console.log("color", color);
+
+//Assignment
+/**
+ * Create a class BookStore, with a constructor that takes "title" and "bookType"
+ * and extra properties called "availableBooks" with initial value of 50.
+ * create a getter methods that will get the book title, bookType and availableBooks.
+ * create custom method called remainingBooks that takes a parameter named "removeBooks".
+ * Create an instances of the book Class with value of choice, log the book "title"
+ * and if for example someone lend three(3) books from BookStore class, then called the
+ * book instance on "availableBooks" to get the number of books available in the store.
+ */
+/**
+ * Principles of OOP
+ * 1. Inheritance
+ * 2. Polymorphism
+ * 3. Abstraction
+ * 4. Encapsulation
+ */
  /* Inheritance
  * A process where multiple classes share properties or methods
  * with their parent class(superclass)
